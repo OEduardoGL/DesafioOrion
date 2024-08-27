0f2a79ef8f67b674a0f85995a818fe53d2c7bcec
Vitor Gouveia <vmcg@ic.ufal.br>
2024-08-27 14:13:09 -0300

    update

--- scripts/csv_to_json.py
+++ scripts/csv_to_json.py
@@ -3,26 +3,21 @@ import json
 import os
 
 
-def to_json(source_file):
-    target_folder = "."
+source_file = "./samples/"
 
-    data = {}
+target_folder = "./converted/"
 
-    with open(source_file, 'r', encoding= "utf-8") as csvfile:
-        csvRead = csv.DictReader(csvfile)
-        k = 0
+data = {}
 
-        for rows in csvRead:
-            data[k] = rows
-            k = k+1
+with open(source_file, 'r', encoding= "utf-8") as csvfile:
+    csvRead = csv.DictReader(csvfile)
+    k = 0
+
+    for rows in csvRead:
+        data[k] = rows
+        k = k+1
 
     file_ = os.path.basename(source_file) + "_converted.json"
 
-    with open(target_folder+file_, 'w', encoding='utf-8') as jsonf:
-            jsonf.write(json.dumps(data, indent=4))
-
-def main(arg, *args):
-    to_csv(arg)
-
-if '__name__' == '__main__':
-    sys.exit(main())
+with open(target_folder+file_, 'w', encoding='utf-8') as jsonf:
+    jsonf.write(json.dumps(data, indent=4))
